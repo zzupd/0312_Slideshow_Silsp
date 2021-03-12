bffd849cd988d10ecb8c3f2b5d769eb3c3243858
zzupd <zzupd@naver.com>
2021-03-12 13:33:25 +0900

    0312_슬라이드쇼_적용완료

---
+++ script/script.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    setInterval(fnSlide, 3000);
+    //             함수명만!,  1/1000
+
+    /*fnSlide();*/    // 함수를 실행시키는 코드(= 함수의 호출, 콜 Call)
+
+    function fnSlide() {
+
+        $("#slideShuttleFrame").animate({
+                "margin-left": "-1000px"
+            },
+            1000,
+            function () {
+
+                // insertAfter 적용위치
+                $("#slideShuttleFrame>a:first-child").insertAfter("#slideShuttleFrame>a:last-child");
+
+                $("#slideShuttleFrame").css({
+                    "margin-left": "0"
+                });
+            });
+
+    }
+
+
+});

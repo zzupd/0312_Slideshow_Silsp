6aa2553d3c1f867b4e76fe22a5998aad956daf94
zzupd <zzupd@naver.com>
2021-03-12 15:04:19 +0900

    0312_개별텍스트적용

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    setInterval(fnSlide, 3000);
+    setInterval(fnSlide, 5000);
     //             함수명만!,  1/1000
 
     /*fnSlide();*/    // 함수를 실행시키는 코드(= 함수의 호출, 콜 Call)
@@ -10,7 +10,7 @@ $(function () {
         $("#slideShuttleFrame").animate({
                 "margin-left": "-1000px"
             },
-            1000,
+            2500,
             function () {
 
                 // insertAfter 적용위치
